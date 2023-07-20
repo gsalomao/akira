efd3269a846d5f328aab6ca38f545ef2005d315b
Gustavo Salomão <gsalomao.eng@gmail.com>
2023-07-20 19:15:59 +0100

    test: store test data for TLS config in separate files

--- listener/tcp_test.go
+++ listener/tcp_test.go
@@ -17,29 +17,12 @@ package listener
 import (
 	"crypto/tls"
 	"net"
+	"os"
 	"testing"
 
 	"github.com/gsalomao/akira"
 	"github.com/stretchr/testify/suite"
 )
-
-const certificate = `-----BEGIN CERTIFICATE-----
-MIIBhTCCASugAwIBAgIQIRi6zePL6mKjOipn+dNuaTAKBggqhkjOPQQDAjASMRAw
-DgYDVQQKEwdBY21lIENvMB4XDTE3MTAyMDE5NDMwNloXDTE4MTAyMDE5NDMwNlow
-EjEQMA4GA1UEChMHQWNtZSBDbzBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABD0d
-7VNhbWvZLWPuj/RtHFjvtJBEwOkhbN/BnnE8rnZR8+sbwnc/KhCk3FhnpHZnQz7B
-5aETbbIgmuvewdjvSBSjYzBhMA4GA1UdDwEB/wQEAwICpDATBgNVHSUEDDAKBggr
-BgEFBQcDATAPBgNVHRMBAf8EBTADAQH/MCkGA1UdEQQiMCCCDmxvY2FsaG9zdDo1
-NDUzgg4xMjcuMC4wLjE6NTQ1MzAKBggqhkjOPQQDAgNIADBFAiEA2zpJEPQyz6/l
-Wf86aX6PepsntZv2GYlA5UpabfT2EZICICpJ5h/iI+i341gBmLiAFQOyTDT+/wQc
-6MF9+Yw1Yy0t
------END CERTIFICATE-----`
-
-const key = `-----BEGIN EC PRIVATE KEY-----
-MHcCAQEEIIrYSSNQFaA2Hwf1duRSxKtLYX5CB04fSeQ6tF1aY/PuoAoGCCqGSM49
-AwEHoUQDQgAEPR3tU2Fta9ktY+6P9G0cWO+0kETA6SFs38GecTyudlHz6xvCdz8q
-EKTcWGekdmdDPsHloRNtsiCa697B2O9IFA==
------END EC PRIVATE KEY-----`
 
 type TCPListenerTestSuite struct {
 	suite.Suite
@@ -62,16 +45,16 @@ func (s *TCPListenerTestSuite) TestListenSuccess() {
 }
 
 func (s *TCPListenerTestSuite) TestListenWithTLSConfigSuccess() {
-	cert, err := tls.X509KeyPair([]byte(certificate), []byte(key))
+	cert, err := os.ReadFile("testdata/test.crt")
 	s.Require().NoError(err)
 
-	tlsConfig := tls.Config{
-		MinVersion:   tls.VersionTLS12,
-		Certificates: []tls.Certificate{cert},
-	}
-	l := NewTCPListener("tcp1", ":1883", &tlsConfig)
-	defer func() { _ = l.Close() }()
+	key, err := os.ReadFile("testdata/test.key")
+	s.Require().NoError(err)
 
+	x509, err := tls.X509KeyPair([]byte(cert), []byte(key))
+	s.Require().NoError(err)
+
+	tlsConfig := tls.Config{MinVersion: tls.VersionTLS12, Certificates: []tls.Certificate{x509}}
 	lsn := NewTCPListener("tcp1", ":1883", &tlsConfig)
 	defer func() { _ = lsn.Close() }()
 
